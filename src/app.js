1c5ea1c1ceb26fab13f0659d5d2f61a77bc35729
Kshipra Malviya <kshipramalviya7@gmail.com>
2023-03-16 23:20:57 +0530

    intial commit

--- src/app.js
+++ src/app.js
@@ -21,10 +21,47 @@ app.set("view engine","hbs");
 app.set("views",templatepath);
 hbs.registerPartials(partialpath);
 
+app.use(express.static(path.join(__dirname, './LoginAuthentication')));
+
+
 // routing
 // app.get(path,callback)
 app.get("/",(req,res)=>{
-    res.render("index");
+    res.sendFile(path.join(__dirname, "./LoginAuthentication/index.html"));
+})
+app.get("/page1.html",(req,res)=>{
+  res.sendFile(path.join(__dirname,"./Home/page1.html"))
+})
+app.get("/page2.html",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./Home/page2.html"))
+  })
+  app.get("/page3.html",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./Home/page3.html"))
+  })
+  app.get("/page4.html",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./Home/page4.html"))
+  })
+  app.get("/page5.html",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./Home/page5.html"))
+  })
+  app.get("/page6.html",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./Home/page6.html"))
+  })
+  app.get("/pacakges.html",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./Home/pacakges.html"))
+  })
+  app.get("/More.html",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./Home/More.html"))
+  })
+  app.get("/form.html",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./Home/form.html"))
+  })
+
+app.get("/authenticated",(req,res)=>{
+    res.sendFile(path.join(__dirname, "./Home/index.html"))
+})
+app.get("/map",(req,res)=>{
+    res.sendFile(path.join(__dirname, "./map/index.html"))
 })
 
 // app.get("/contact",(req,res)=>{
